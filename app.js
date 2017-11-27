8f736ff38b5da2a762013061a810c0052a97e3d8
JeffMatthews2 <jeff@superdocs.com>
2017-11-26 18:01:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
 // Connect to DB
-mongoose.connect('mongodb://admin:bball@ds119406.mlab.com:19406/basketball');
+mongoose.connect('mongodb://[user]:[pw]@[db]');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
